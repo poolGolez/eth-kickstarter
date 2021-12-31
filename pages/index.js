cd1de24a7b4ee11ee2c1df18e9f7afc53e71c8e0
pool <pool@skipapp.com.au>
2021-12-31 23:21:09 +0800

    Add cards for campaign list

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
+import { Button, Icon, Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class CampaignIndex extends Component {
 
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-
         return { campaigns }
     }
 
-    async componentDidMount() {
-        console.log("Factory:", factory);
+    renderCampaigns() {
+        const items = this.props.campaigns.map((address) => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        })
+        return <Card.Group items={items} />;
     }
 
     render () {
-        return (<h1>Oh shit! { this.props.campaigns[0] }</h1>);
+        return <div>
+            <h3>Open Campaigns</h3>
+            { this.renderCampaigns() }
+            <Button icon primary labelPosition='left'>
+                <Icon name='add'/> Create Campaign
+            </Button>
+        </div>
     }
 }
 
