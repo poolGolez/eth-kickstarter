17e71cf3a6443b9b65f9126df46578ff9eadafc6
pool <pool@skipapp.com.au>
2021-12-31 22:07:44 +0800

    Load all deployed campaigns at kiad

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,18 @@ import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
 
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+        return { campaigns }
+    }
+
     async componentDidMount() {
         console.log("Factory:", factory);
     }
 
     render () {
-        return (<h1>Index page??? Oh shit!</h1>);
+        return (<h1>Oh shit! { this.props.campaigns[0] }</h1>);
     }
 }
 
