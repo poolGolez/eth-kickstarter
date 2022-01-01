90e58ec356ccc00f2e4d3a0ee7cdee8acf5f9568
pool <loop.edward@gmail.com>
2022-01-01 12:53:09 +0800

    Fix campaign export

--- ethereum/campaign.js
+++ ethereum/campaign.js
@@ -2,6 +2,6 @@
 import web3 from "./web3";
 import compiledCampaign from "./build/Campaign.json";
 
-export default async(address) => {
+export default (address) => {
     return new web3.eth.Contract(JSON.parse(compiledCampaign.interface), address);
 };
