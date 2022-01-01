191cc5d35408bbd26f9b08ccc6ec1a4f04bcf020
pool <pool@skipapp.com.au>
2022-01-01 10:17:33 +0800

    Add error handling and form responsiveness

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Button, Form, Icon, Input, Label } from "semantic-ui-react";
+import { Button, Form, Icon, Input, Label, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import CampaignFactory from "../../ethereum/factory";
 import web3 from '../../ethereum/web3';
@@ -7,12 +7,13 @@ import web3 from '../../ethereum/web3';
 class CampaignsNew extends Component {
     state = {
         minContribution: '250',
-        errorMessage: ''
+        errorMessage: '',
+        submitting: false
     }
 
     onSubmit = async (event) => {
+        this.setState({ submitting: true, errorMessage: '' });
         event.preventDefault();
-
         try {
             const accounts = await web3.eth.getAccounts();
             await CampaignFactory.methods
@@ -24,14 +25,17 @@ class CampaignsNew extends Component {
         } catch(err) {
             this.setState({ errorMessage: err.message });
         }
-
+        this.setState({ submitting: false });
     };
 
     render() {
         return (
             <Layout>
                 <h3>Create a new campaign</h3>
-                <Form onSubmit={ this.onSubmit }>
+                <Form
+                    loading={ this.state.submitting }
+                    onSubmit={ this.onSubmit }
+                    error={this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -40,6 +44,12 @@ class CampaignsNew extends Component {
                             onChange={ event => this.setState({minContribution: event.target.value}) }
                             value={this.state.minContribution}/>
                     </Form.Field>
+
+                    <Message
+                        error
+                        header='Something went wrong'
+                        content={this.state.errorMessage}
+                    />
                     <Button
                         type='submit'
                         icon
