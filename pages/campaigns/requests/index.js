b5247d6daf55de4a85fc57cd3ceb5c93fe11f8f9
pool <loop.edward@gmail.com>
2022-01-02 18:38:41 +0800

    Create initial listing of budgets

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,25 @@
-import { Button, Icon, Label, Menu, Table } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
+import BudgetsTable from "../../../components/BudgetsTable";
+import Campaign from "../../../ethereum/campaign";
 import { Link } from '../../../routes';
 
 const { Component } = require("react");
 
 class RequestsIndex extends Component {
 
-    static getInitialProps(props) {
+    static async getInitialProps(props) {
         const campaignAddress = props.query.address;
-        return { campaignAddress };
+        const campaignContract = Campaign(campaignAddress);
+        const budgetsCount = parseInt(await campaignContract.methods
+                                .getBudgetsCount()
+                                .call());
+
+        const budgets = await Promise.all(Array(budgetsCount).fill().map((_, index) => {
+                            return campaignContract.methods.budgets(index).call();
+                        }));
+
+        return { campaignAddress, budgets };
     }
 
     render() {
@@ -17,54 +28,11 @@ class RequestsIndex extends Component {
                 <Link route={`/campaigns/${this.props.campaignAddress}/requests/new`}>
                     <Button
                         floated="right"
-                        content="Add Request"
+                        content="Request new budget"
                         primary />
                 </Link>
                 <h3>Requests</h3>
-                <Table celled>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>ID</Table.HeaderCell>
-                            <Table.HeaderCell>Description</Table.HeaderCell>
-                            <Table.HeaderCell>Amount</Table.HeaderCell>
-                            <Table.HeaderCell>Recipient</Table.HeaderCell>
-                            <Table.HeaderCell>Approval Count</Table.HeaderCell>
-                            <Table.HeaderCell>Action</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-
-                    <Table.Body>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Label ribbon>First</Label>
-                            </Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                            <Table.Cell>Cell</Table.Cell>
-                        </Table.Row>
-                    </Table.Body>
-
-                    <Table.Footer>
-                        <Table.Row>
-                            <Table.HeaderCell colSpan='6'>
-                            <Menu floated='right' pagination>
-                                <Menu.Item as='a' icon>
-                                    <Icon name='chevron left' />
-                                </Menu.Item>
-                                <Menu.Item as='a'>1</Menu.Item>
-                                <Menu.Item as='a'>2</Menu.Item>
-                                <Menu.Item as='a'>3</Menu.Item>
-                                <Menu.Item as='a'>4</Menu.Item>
-                                <Menu.Item as='a' icon>
-                                    <Icon name='chevron right' />
-                                </Menu.Item>
-                            </Menu>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Footer>
-                </Table>
+                <BudgetsTable budgets={ this.props.budgets }></BudgetsTable>
             </Layout>
         );
     }
